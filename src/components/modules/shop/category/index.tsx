f40e02c4760af30ec286d06c1dbdf1fbd597ab97
Md ashiquzzaman <ahmedashik18k@gmail.com>
2025-11-02 22:56:20 +0600

    category page setup done

---
+++ src/components/modules/shop/category/index.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+
+const ManageCategories = () => {
+  return (
+    <div>
+      <div className="flex justify-between">
+        <h2>Manage Category </h2>
+        <Button>Create category</Button>
+      </div>
+    </div>
+  );
+};
+
+export default ManageCategories;
