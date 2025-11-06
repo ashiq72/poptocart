408401835db7066f8c6061c1a09912c91c5eee5a
Md ashiquzzaman <ahmedashik18k@gmail.com>
2025-11-06 13:46:48 +0600

    category delete added

---
+++ src/app/(withDashboardLayout)/user/shop/brand/page.tsx
@@ -0,0 +1,9 @@
+// import ManageCategories from "@/components/modules/shop/category";
+// import { getAllCategories } from "@/services/Category";
+
+const ProductBrandPage = async () => {
+  //   const { data, meta } = await getAllCategories();
+  return <div>{/* <ManageCategories categories={data} /> */}ss</div>;
+};
+
+export default ProductBrandPage;
